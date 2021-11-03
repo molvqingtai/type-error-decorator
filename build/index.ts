24a525f0f12dfca3fde4bacba182aaa4243d7406
molvqingtai <molvqingtai@gmail.com>
2021-11-03 19:08:30 +0800

    ci: optimized build script

--- build/index.ts
+++ build/index.ts
@@ -1,16 +1,22 @@
 import esbuild from 'esbuild'
+import { $ } from 'zx'
 
 const isDevelop = process.env.NODE_ENV === 'develop'
 
-esbuild
-  .build({
-    entryPoints: ['src/index.ts'],
-    outdir: 'dist',
-    target: 'esnext',
-    format: 'esm',
-    bundle: true,
-    watch: isDevelop,
-    sourcemap: isDevelop,
-    minify: !isDevelop
-  })
-  .catch(() => process.exit(1))
+void (async () => {
+  await $`rm -rf dist`
+  await esbuild
+    .build({
+      entryPoints: ['src/index.ts'],
+      outdir: 'dist',
+      target: 'esnext',
+      format: 'esm',
+      bundle: true,
+      watch: isDevelop,
+      sourcemap: isDevelop,
+      minify: !isDevelop
+    })
+    .then(async (res) => {
+      isDevelop ? await $`tsc --watch` : await $`tsc`
+    })
+})().catch(() => process.exit(1))
