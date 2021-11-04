1fd6bbf355b052dce9d4124c830d48088c4d5130
molvqingtai <molvqingtai@gmail.com>
2021-11-05 04:08:38 +0800

    perf: optimize decorator name definition

--- src/index.ts
+++ src/index.ts
@@ -21,13 +21,13 @@ const paramsChecker = (models: Model[], params: any[], prefix: string) =>
       throw new TypeError(`${prefix} arguments[${model.index}] is required`)
     }
     if (typeof value !== 'undefined' && !types.some((type: Constructor) => instanceEqual(type, value))) {
-      const typeString = types.map((type: Constructor) => type.name.toString()).toString()
+      const typeString = types.map((type: Constructor) => type.name).toString()
       throw new TypeError(`${prefix} arguments[${model.index}] must be ${typeString}`)
     }
   })
 
 export const TypeClass = <T extends Constructor>(constructor: T) => {
-  const models = Reflect.getMetadata(METADATA_KEY, constructor, 'Param').get('constructor')
+  const models = Reflect.getMetadata(METADATA_KEY, constructor, 'TypeParam').get('constructor')
   return class extends constructor {
     constructor(...args: any[]) {
       paramsChecker([...models].reverse(), args, 'constructor')
@@ -38,18 +38,18 @@ export const TypeClass = <T extends Constructor>(constructor: T) => {
 
 export const TypeMethod = (target: object, key: string, descriptor: PropertyDescriptor) => {
   const originalMethod = descriptor.value
-  const models = Reflect.getMetadata(METADATA_KEY, target, 'Param').get('method')
+  const models = Reflect.getMetadata(METADATA_KEY, target, 'TypeParam').get('method')
   descriptor.value = function (...args: any[]) {
     paramsChecker([...models].reverse(), args, key)
     return originalMethod.apply(this, args)
   }
 }
 
-export const Param =
+export const TypeParam =
   (type: Constructor | Constructor[], required?: boolean) => (target: object, key: string, index: number) => {
     const _ModelMap = ModelMap.set(
       key ? 'method' : `constructor`,
       ModelMap.get(key)?.add({ type, required, index }) ?? new Set([{ type, required, index }])
     )
-    Reflect.defineMetadata(METADATA_KEY, _ModelMap, target, 'Param')
+    Reflect.defineMetadata(METADATA_KEY, _ModelMap, target, 'TypeParam')
   }
