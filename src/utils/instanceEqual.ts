462038ddc14a2bca130bd46c99cca998e5e14b2a
molvqingtai <molvqingtai@gmail.com>
2021-11-05 01:51:21 +0800

    ci: delete excess script

--- src/utils/instanceEqual.ts
+++ src/utils/instanceEqual.ts
@@ -1,5 +1,10 @@
 import typeOf from './typeOf'
 
+/**
+ * instanceEqual
+ * Because compressed code may change function name
+ * Only built-in constructors can be compared
+ */
 const instanceEqual = (a: any, b: any) => {
   if (typeof a === 'function' && typeof b === 'function') {
     return a.name === b.name
